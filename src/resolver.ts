7a29d8106eee670a66ffeffa0ed0804a3c19e1d1
shahreaz0 <ashahreaz@gmail.com>
2023-10-16 02:27:40 +0600

    Mutation

--- src/resolver.ts
+++ src/resolver.ts
@@ -1,6 +1,4 @@
-import db from "../db.json";
-
-import {} from "@apollo/server";
+import db from "../db";
 
 export const resolvers = {
   Query: {
@@ -24,5 +22,47 @@ export const resolvers = {
   Author: {
     reviews: (parent: { id: string }) =>
       db.reviews.filter((review) => review.author_id === parent.id),
+  },
+
+  Mutation: {
+    addGame: async (
+      _: any,
+      args: { newGame: { id: string; title: string; platform: string[] } }
+    ) => {
+      db.games.push(args.newGame);
+
+      return args.newGame;
+    },
+    deleteGame: async (_: any, args: { id: string }) => {
+      const deleted = db.games.find((game) => game.id === args.id);
+
+      console.log({ args, deleted });
+
+      const filtered = db.games.filter((game) => game.id !== args.id);
+
+      db.games = [...filtered];
+
+      return deleted;
+    },
+
+    updateGame: (
+      _: any,
+      args: { id: any; game: { title: string; platform: string[] } }
+    ) => {
+      db.games = db.games.map((game) => {
+        if (game.id === args.id) {
+          return {
+            ...game,
+            ...args.game,
+          };
+        }
+
+        return {
+          ...game,
+        };
+      });
+
+      return db.games.find((game) => game.id === args.id);
+    },
   },
 };
--- src/schema.ts
+++ src/schema.ts
@@ -30,4 +30,21 @@ export const typeDefs = `#graphql
         authors: [Author]
     }
 
+    type Mutation {
+        addGame(newGame: GameInput!): Game
+        deleteGame(id: ID): Game
+        updateGame(id: ID!, game:GameUpdateInput!): Game
+    }
+
+    input GameInput {
+        id: ID!
+        title: String!
+        platform: [String!]!
+    }
+
+    input GameUpdateInput {
+        title: String
+        platform: [String]
+    }
+
 `;
